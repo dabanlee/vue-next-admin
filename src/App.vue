166f798a13a6eb6ed41d912b853fc200ca6edce1
大板栗 <576839360@qq.com>
2021-03-31 10:10:34 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: `App`,
+})
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/Component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Component"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Component',
+});
+</script>
+
+<style scoped lang="scss">
+// .Component {}
+</style>
