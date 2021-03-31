6969d7fb9a990c92ef34cb478af2f285600055e9
大板栗 <576839360@qq.com>
2021-03-31 10:54:07 +0800

    chore: use vuex modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
 import { createStore } from 'vuex'
 
+const files = require.context('./modules', true, /\.ts$/)
+
+const modules = files.keys().reduce((modules: { [key: string]: any }, path) => {
+  const name = path.replace(/^\.\/(.*)\.\w+$/, '$1')
+  modules[name] = files(path).default
+  return modules
+}, {})
+
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  modules,
 })
